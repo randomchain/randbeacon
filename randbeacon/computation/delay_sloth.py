4dbdbe52cca7e06508507c25bc3b1639b638ce1e
Mathias Sass Michno <m@michno.me>
2018-04-11 10:45:28 +0200

    add pub to computation

--- randbeacon/computation/delay_sloth.py
+++ randbeacon/computation/delay_sloth.py
@@ -26,11 +26,11 @@ def init_compute(inp_data):
     return sloth
 
 
-def start_compute_loop(pub, pull, timeout=5):
+def start_compute_loop(process_pub, pub, pull, timeout=5):
     log.info("Starting compute loop")
     while True:
         log.info("publishing process message")
-        pub.send_multipart([b'process', b'meme'])
+        process_pub.send_multipart([b'process', b'meme'])
         log.info("waiting {} seconds for incoming data".format(timeout))
         if pull.poll(timeout * 1000):
             header, seq_no_a, inp_data = pull.recv_multipart()
@@ -39,19 +39,19 @@ def start_compute_loop(pub, pull, timeout=5):
                 log.error("Expected to receive compute data, not commitment")
                 return
             header, seq_no_b, commit_data = pull.recv_multipart()
-            commit_data = msgpack.unpackb(commit_data)
-            log.info("recv -> {} | {} | merkle height {}".format(header, seq_no_b, len(commit_data)))
+            commit_data_obj = msgpack.unpackb(commit_data)
+            log.info("recv -> {} | {} | merkle height {}".format(header, seq_no_b, len(commit_data_obj)))
             if header != b'\x02':
                 log.error("Expected to receive commitment, not compute data")
                 return
             if seq_no_a != seq_no_b:
                 log.error("Got unpaired sequence numbers!")
                 return
-            # forward commitment to publisher
-
-            # in the future we may want to change the order at which this happens
             sloth = init_compute(inp_data)
+            pub.send_multipart([header, seq_no_b, commit_data])
             sloth.wait()
+            pub.send_multipart([b'\x03', seq_no_b, sloth.final_hash])
+            pub.send_multipart([b'\x04', seq_no_b, sloth.witness])
             log.info(" === OUTPUT --> {} === ".format(sloth.final_hash))
             log.info(" === WITNESS --> {} === ".format(sloth.witness))
 
@@ -59,10 +59,11 @@ def start_compute_loop(pub, pull, timeout=5):
 
 @click.command()
 @click.option('--pull-bind', default="tcp://*:11234", help="Pull socket bind")
-@click.option('--pub-bind', default="tcp://*:23456", help="Publish socket bind")
+@click.option('--pub-bind', default="tcp://*:33456", help="Publish socket bind")
+@click.option('--process-pub-bind', default="tcp://*:23456", help="Input processor Publish socket bind")
 @click.option('--sloth-bits', default=2048, help="Bits for sloth prime")
 @click.option('--sloth-iterations', default=5000, help="Iterations of permutation in sloth")
-def main(pull_bind, pub_bind, sloth_bits, sloth_iterations):
+def main(pull_bind, pub_bind, process_pub_bind, sloth_bits, sloth_iterations):
     global SLOTH_BITS, SLOTH_ITERATIONS
 
     SLOTH_BITS = sloth_bits
@@ -71,11 +72,14 @@ def main(pull_bind, pub_bind, sloth_bits, sloth_iterations):
     pull = ctx.socket(zmq.PULL)
     pull.bind('tcp://*:11234')
 
+    process_pub = ctx.socket(zmq.PUB)
+    process_pub.bind('tcp://*:23456')
+
     pub = ctx.socket(zmq.PUB)
-    pub.bind('tcp://*:23456')
+    pub.bind('tcp://*:33456')
 
     time.sleep(1)
-    start_compute_loop(pub, pull)
+    start_compute_loop(process_pub, pub, pull)
 
 
 if __name__ == "__main__":
